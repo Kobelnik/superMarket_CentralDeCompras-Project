5e57c7753a975f956f2529f7482465bbdd03ab3e
Kobelnik <luisfeliperkobelnik@gmail.com>
2025-11-05 17:35:38 -0300

    feat: Criacao do botao da home para a createProduct

---
+++ supermarket_TudoTem/src/pages/CreateProduct.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Aqul vamos criar o produto</h1>
+</template>
+
+<script setup lang=""ts>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ supermarket_TudoTem/src/pages/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <button @click="goToCreateProduct"> Criar Anúncio</button>
+    </div>
+</template>
+
+
+<script setup lang=""ts>
+    import {useRouter} from 'vue-router';
+    const router = useRouter();
+
+    function goToCreateProduct() {
+        router.push('/create-product');
+    }
+
+</script>
+
+
+<style scoped>
+
+</style>
